b4b12f1e3ace0320b4f18330853d1a22ff62b51a
Pim Vermeer <pim.vermeer@gmail.com>
2025-03-09 13:13:33 +0000

    fix(self-update): check if commits are the same before update

--- scripts/updater.sh
+++ scripts/updater.sh
@@ -3,7 +3,7 @@
 update_self() {
   if ! which git >/dev/null; then
     echo_warning "Git not installed, not able to self update"
-    return
+    return 1
   fi
 
   local git_repo="https://github.com/PVermeer/rpm-tools"
@@ -12,17 +12,27 @@ update_self() {
 
   if [ -z "$rpm_tools_submodule" ]; then
     echo "Not installed as submodule, not able to self update"
-    return
+    return 1
   fi
 
   if [ -n "$rpm_tools_submodule" ]; then
-    if git submodule update --init --remote -f $rpm_tools_submodule; then
-      echo "Updated myself to latest git"
-    else
-      echo_warning "Could not update myself"
+    local current_commit=$(git submodule status | grep $rpm_tools_submodule | awk '{ print $1 }')
+    # Remove leading '+'' if submodule update is not commited yet
+    current_commit=$(echo $current_commit | sed "s/\+/""/")
+
+    local latest_commit=$(git ls-remote "$git_repo" "HEAD" | awk '{ print $1 }')
+
+    if [ "$current_commit" != "$latest_commit" ]; then
+      if git submodule update --init --remote -f $rpm_tools_submodule; then
+        echo "Updated myself to latest git"
+        return 0
+      else
+        echo_warning "Could not update myself"
+        return 1
+      fi
     fi
-    return
   fi
 
   echo "No update found"
+  return 1
 }
