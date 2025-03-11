516735bf69e7db6f89641933fc8fa383d9d84831
Pim Vermeer <pim.vermeer@gmail.com>
2025-03-11 09:16:11 +0000

    fix(deps): missing dependencies
    Ci does not have them

--- scripts/utils.sh
+++ scripts/utils.sh
@@ -6,13 +6,17 @@ fail() {
 }
 
 install_dependencies() {
-  if ! which getopt &>/dev/null; then
-    echo_color "Installing util-linux"
-    run_debug sudo dnf install util-linux -y --quiet || return 1
+  local missing_deps=""
+  if ! which getopt &>/dev/null; then missing_deps+=" util-linux"; fi
+  if ! which git &>/dev/null; then missing_deps+=" git"; fi
+  if ! which jq &>/dev/null; then missing_deps+=" jq"; fi
+
+  if [ -n "$missing_deps" ]; then
+    echo -n "Installing packages: "
+    echo_color "$missing_deps:"
     echo ""
-  fi
-    if ! which git &>/dev/null; then
-    echo_color "Installing git"
-    run_debug sudo dnf install git -y --quiet || return 1
+    run_debug sudo dnf install -y --quiet $missing_deps || return 1
+  else
+    echo "No missing packages"
   fi
 }
